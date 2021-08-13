8505d7673ad8a23677fbdf5f3a0980c6154fc4d4
Lauren Ransohoff <lauren.ransohoff@gmail.com>
2021-08-13 19:10:06 -0400

    Added capability to reduce number of colors using kmeans clustering, then assigned dmc colors to new cluster centroids. And sunflower input image

--- Pattern/pattern.py
+++ Pattern/pattern.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.spatial import distance
+from sklearn.cluster import KMeans
 
 class Pattern:
     def __init__(self, img, num_colors):
@@ -12,24 +13,17 @@ class Pattern:
         clr_df = pd.read_csv(r'C:\Users\laure\Documents\Lauren Folder\Python Scripts\CrossStitchPatternGen\DMCColorMap.csv')
         self.color_map = clr_df
         
-    def assign_colors(self):
-        n, m, _ = self.img.shape
+    def assign_colors(self, img):
+        n, m, _ = img.shape
         dmc_rgb = np.uint8(np.zeros((n, m, 3)))
         dmc_colors = []
         r, c = self.color_map.shape
         rbg_cm_array = self.color_map[['Red','Green','Blue']].to_numpy()
         for i in range(0, n):
             for j in range(0, m):
-                temp_rgb = self.img[i,j]
+                temp_rgb = img[i,j]
                 d_cell = distance.cdist([temp_rgb],rbg_cm_array,'euclidean')
                 ind_best = np.argmin(d_cell)
-#                min_dist = 255
-#                ind_best = -1
-#                for k in range(0, r):
-#                    cm_temp = [self.color_map.Red[k], self.color_map.Green[k], self.color_map.Blue[k]]
-#                    if np.sqrt(np.sum((temp_rgb - cm_temp)**2, axis=0)) < min_dist:
-#                        min_dist = np.sqrt(np.sum((temp_rgb - cm_temp)**2, axis=0))
-#                        ind_best = k
                 dmc_rgb[i, j, :] = [self.color_map.Red[ind_best], 
                         self.color_map.Green[ind_best], 
                         self.color_map.Blue[ind_best]]
@@ -37,6 +31,37 @@ class Pattern:
 #        print(dmc_colors)
         return dmc_rgb
     
+    def reduce_colors(self, img):
+        # Get starting shape of image
+        n, m, q = self.img.shape
+        # Reshape dmc colors image map to an x by 3 (q=3) 2d array
+        dmc_colors_array = img.reshape(-1,q)
+        # Use numpy unique to find list of unique colors (each color is a 1x3 
+        #  row of the dmc_colors_array, so do axis=0 to get unique rows instead of single values)
+        [unique_dmc_colors, inv] = np.unique(dmc_colors_array, return_inverse=True ,axis=0)
+        # Now we need to reduce the number of unique colors
+        # How to do this....
+        # Try using k means clustering! Fun!
+        # Thanks Jeremy -https://www.jeremyjordan.me/grouping-data-points-with-k-means-clustering/
+        k = self.num_colors
+        kmeans = KMeans(n_clusters = k)
+        kmeans.fit(unique_dmc_colors)
+        kmeans.labels_
+        # Now we have our k groups - time to find k colors
+        # Take centroid of each group and then find closest dmc color to the centroid?
+        # Let's try it!
+        color_assignment = dict()
+        color_assignment["first_dmc_color"] = unique_dmc_colors
+        color_assignment["kmeans_group"] = kmeans.labels_
+        color_assignment["new_rgb_color"] = kmeans.cluster_centers_[kmeans.labels_]
+        
+        new_rgb_flat = color_assignment["new_rgb_color"][inv]
+        
+        new_rgb_img = new_rgb_flat.reshape(n, m, q)
+        
+        new_dmc = self.assign_colors(new_rgb_img)
+        return new_dmc
+        
     def display_img(self, img):
         fig, axs = plt.subplots(1, 1, figsize=(5,5))
         axs.imshow(img)
--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from Image.image import Image
 from Pattern.pattern import Pattern
 
 def run():
-    in_path = r'C:\Users\laure\Documents\Lauren Folder\Python Scripts\CrossStitchPatternGen\IMG_8936.JPG'
-    n_windows = 50
+    in_path = r'C:\Users\laure\Documents\Lauren Folder\Python Scripts\CrossStitchPatternGen\sunflower.JPG'
+    n_windows = 100
     i = Image(import_path = in_path,
               num_windows = n_windows)
     img = i.import_image()
@@ -14,12 +14,14 @@ def run():
     i.display_img(img)
     i.display_img(img_new)
     
-    n_colors = 7
+    n_colors = 20
     p = Pattern(img = img_new,
                 num_colors = n_colors)
     p.import_colormap()
-    dmc_rgb = p.assign_colors()
+    dmc_rgb = p.assign_colors(img_new)
     p.display_img(dmc_rgb)
+    dmc_new = p.reduce_colors(dmc_rgb)
+    p.display_img(dmc_new)
     
 if __name__ == '__main__':
     run()
